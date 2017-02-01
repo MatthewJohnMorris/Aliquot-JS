d725ae8ee63e9b4b9f702f17364c75bc836986ee
Matthew Morris <matt_morris_zz@yahoo.co.uk>
2017-02-01 23:30:49 +0000

    minor cleanup in primes generation code

--- html/primes-runner.js
+++ html/primes-runner.js
@@ -9,6 +9,7 @@ importScripts('primes.js');
 
 var top_num = 4294967291; // 1000000000; // 4294967291; // 2147483647; // 1000000000;
 var cnt = 0;
+var timeStart = new Date().getTime();
 
 postMessage('initialising: top_num = ' + top_num);
 
@@ -16,11 +17,13 @@ var gen = new SoEPgClass();
 var p = 1;
 while ((p = gen.next()) <= top_num) {
   cnt++;
-  if(cnt % 10000 == 0) {
+  if(cnt % 100000 == 0) {
     var percent = Math.floor(100 * p / top_num);
-    postMessage({cnt:cnt, percent:percent, top_num:top_num, p:p});
+    var elapsed = Math.floor(((new Date()).getTime() - timeStart) / 1000);
+    postMessage({lowi: gen.lowi, elapsed:elapsed, cnt:cnt, percent:percent, top_num:top_num, p:p});
   }
 }
 var percent = Math.floor(100 * p / top_num);
-postMessage({cnt:cnt, percent:percent, top_num:top_num, p:p});
+var elapsed = Math.floor(((new Date()).getTime() - timeStart) / 1000);
+postMessage({lowi: gen.lowi, elapsed:elapsed, cnt:cnt, percent:percent, top_num:top_num, p:p});
 
