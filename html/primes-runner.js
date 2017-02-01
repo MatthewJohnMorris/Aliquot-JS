12ec326c4591c1adc3c0799546bd47c7e5d29f33
Matthew Morris <matt_morris_zz@yahoo.co.uk>
2017-02-01 22:47:45 +0000

    background primes

---
+++ html/primes-runner.js
@@ -0,0 +1,26 @@
+  // see https://primes.utm.edu/howmany.html
+  // see http://www.umopit.ru/CompLab/primes32eng.htm
+  //  1000000000 ok, should be 50847534 (11,314ms)
+  //  2147483647 ?
+  //  4294967291 ok, should be 203280221 (216,090ms)
+  // 10000000000 not ok, should be 455052511 	 
+
+importScripts('primes.js');
+
+var top_num = 4294967291; // 1000000000; // 4294967291; // 2147483647; // 1000000000;
+var cnt = 0;
+
+postMessage('initialising: top_num = ' + top_num);
+
+var gen = new SoEPgClass();
+var p = 1;
+while ((p = gen.next()) <= top_num) {
+  cnt++;
+  if(cnt % 10000 == 0) {
+    var percent = Math.floor(100 * p / top_num);
+    postMessage({cnt:cnt, percent:percent, top_num:top_num, p:p});
+  }
+}
+var percent = Math.floor(100 * p / top_num);
+postMessage({cnt:cnt, percent:percent, top_num:top_num, p:p});
+
